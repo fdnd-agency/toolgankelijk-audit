6f85ed7bdbbffc24a75a3032e71492f6e5d453b7
Bjarne Zeeman <bjarne.zeeman@hva.nl>
2025-06-02 15:46:54 +0200

    Revert "specificeer poort"
    This reverts commit ec573ffaa7f7a780cf4bf030c900c3049d22d1c8.

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,9 @@
 import adapter from '@sveltejs/adapter-node';
-import { PORT } from '$env/static/private';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
-		vite: () => ({ server: { port: PORT } })
+		adapter: adapter()
 	}
 };
 
