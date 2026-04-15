f9a3e21b2373b10033f37974085ad8367477be3d
MaksimH2O <93390095+MaksimH2O@users.noreply.github.com>
2026-04-15 11:41:27 +0200

    refactor: Moved saveCheck logic into auditService; AuditService now uses updated repositories.

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,16 +1,39 @@
 // place files you want to import through the `$lib` alias in this folder.
+import { getDirectusClient } from './server/utils/Directus.js';
+import { UrlRepository } from './server/repositories/UrlRepository.js';
+import { CheckRepository } from './server/repositories/CheckRepository.js';
+import { SuccessCriteriaRepository } from './server/repositories/SuccessCriteriaRepository.js';
+import { CheckCriteriaLinkRepository } from './server/repositories/CheckCriteriaLinkRepository.js';
+import { WebsiteRepository } from './server/repositories/WebsiteRepository.js';
+import { TestResultRepository } from './server/repositories/TestResultRepository.js';
+import { TestNodeRepository } from './server/repositories/TestNodeRepository.js';
+import { AuditService } from './server/services/AuditService.js';
+
 export { default as ActiveAudits } from './server/utils/ActiveAudits.js';
-export { default as postTestResult } from './server/repositories/queries/postTestResult.js';
-export { default as postTestNode } from './server/repositories/queries/postTestNode.js';
-export { default as getQueryToolboard } from './server/repositories/queries/getToolboardData.js';
-export { default as getFirstCheck } from './server/repositories/queries/firstCheck.js';
-export { default as getSuccesscriteriumByIndex } from './server/repositories/queries/getIndexId.js';
-export { default as addCheck } from './server/repositories/queries/addCheck.js';
-export { default as deleteCheck } from './server/repositories/queries/deleteCheck.js';
-export { default as getAllPartnersWithTheirUrls } from './server/repositories/queries/getAllPartnersWithUrls.js';
-export { hygraph } from './server/utils/Hygraph.js';
+export { getDirectusClient };
 export { Partner } from './server/models/Partner.js';
 export { TestResult } from './server/models/TestResult.js';
 export { TestNode } from './server/models/TestNode.js';
 export { runAuditForUrl } from './server/utils/AuditRunner.js';
 export { requestWithRetry } from './server/utils/RequestRetry.js';
+
+const client = getDirectusClient();
+const repositoryDependencies = { client };
+// Initialize repositories
+export const urlRepository = new UrlRepository(repositoryDependencies);
+export const checkRepository = new CheckRepository(repositoryDependencies);
+export const successCriteriaRepository = new SuccessCriteriaRepository(repositoryDependencies);
+export const checkCriteriaLinkRepository = new CheckCriteriaLinkRepository(repositoryDependencies);
+export const websiteRepository = new WebsiteRepository(repositoryDependencies);
+export const testResultRepository = new TestResultRepository(repositoryDependencies);
+export const testNodeRepository = new TestNodeRepository(repositoryDependencies);
+// Initialize audit service
+export const auditService = new AuditService({
+	websiteRepository,
+	urlRepository,
+	checkRepository,
+	successCriteriaRepository,
+	checkCriteriaLinkRepository,
+	testResultRepository,
+	testNodeRepository
+});
